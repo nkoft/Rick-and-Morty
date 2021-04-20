ea591380725a5ffe126bb465cc45301fbfb1064a
Nick Kofteris <niko@Nicks-Air.lan>
2021-04-20 17:00:25 -0400

    select character form

--- apps.js
+++ apps.js
@@ -7,14 +7,9 @@ async function getCharacterData() {
 
   try {
     const response = await axios.get(url)
-    console.log(response.data.results)
+    // console.log(response.data.results)
+    getCharacters(response.data.results)
     return response
-
-
-    // for (let i = 0; i < response.data.results.length; i++) {
-    //   console.log(characterList[i])
-    // }
-
 
   }
   catch (error) {
@@ -24,38 +19,63 @@ async function getCharacterData() {
 
 getCharacterData()
 
-
+function getCharacters(data) {
+  // console.log('characters', data)
+  const characterSelect = document.querySelector('#select-character')
+  // console.log(characterForm)
+  data.forEach((person) => {
+    // console.log(person.id)
+    const personTag = document.createElement('option')
+    personTag.textContent = person.name
+    personTag.value = person.id
+    characterSelect.append(personTag)
+  })
+}
 
 // Create the form option tags
 // One for all characters, one for status(dead or alive), one for species
-function setCharacterOptions(list) {
-  // console.log(list)
-  const selectCharacterTag = document.querySelector('#select-character')
-  list.forEach((name) => {
-    // console.log(name)
-    const characterOptionTag = document.createElement('option')
-    characterOptionTag.textContent = name
-    characterOptionTag.value = name
-    selectCharacterTag.append(characterOptionTag)
-  });
-  return list
+// 
+
+async function getCharacterValue(id) {
+  try {
+    const url = `https://rickandmortyapi.com/api/character/${id}`
+    const response = await axios.get(url)
+    console.log(response.data)
+
+
+    // invoke get character div function here ***
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
 
-// let characterList = Object.keys(response.data.results)
-// console.log(characterList)
-// setCharacterOptions(characterList)
-// return characterList
+
+const characterForm = document.querySelector('#person-form')
+characterForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const selectCharacter = document.querySelector('#select-character').value
+  // console.log(selectCharacter)
+  getCharacterValue(selectCharacter)
+
+})
+
+
+// Surely this is incorrect:
+
+// const getCharacterSection = (data) => {
+//   data.forEach((character) => {
+//     const characterName = document.createElement('h2')
+//     characterName.innerText = character.name
+//     document.querySelector('.characters').append(characterName)
+//   })
+// }
 
 
 
 // Get option tag values:
 // Create tag value for characters
-function getValue(e) {
-  e.preventDefault()
-  const characterOptionValue = document.querySelector('#select-character').value
-  // console.log(characterOptionValue)
-  getBreedImage(characterOptionValue)
-}
+
 
 
 // Create tag value for species
